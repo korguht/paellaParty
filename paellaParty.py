9d5e19317f147600ee7597eb996953dba4c08a59
bgcook@users.noreply.github.com <bcookmail@gmail.com>
2017-09-26 14:09:58 -0400

    Initial commit

--- paellaParty.py
+++ paellaParty.py
@@ -1,44 +1,48 @@
 """
-paellaParty!
-Notifies user when information is available...
+paellaParty! Rev.1
+Monitors a twitter account id for tweets containing a specified keyword and recieve a text message
+when/if a match is found.
+
+Originally created
 """
 
 import twitter
 import time
 from twilio.rest import Client
+from keys import *
 
-accountSID = "AC1fa156de65574a2d9d4843cc33924495"
-authToken = "f5aa815c32c9fc6035f1d13b89d7e4a9"
-myTwilNum = "+13216167352"
-myCellNum = "+13217204928"
 
+# keys are specified and imported from keys.py
 twilioCli = Client(accountSID, authToken)
-
-api = twitter.Api(consumer_key="vHRc2kmvVGlbSQDCLWPs2ifKL",
-                  consumer_secret="skedX80bprCPehS9RMfhDzKlE0ArzUvDs9ZkmlINW6YCAvHo0U",
-                  access_token_key="15213778-5WTwUXaba0fwkslchwArg47HjkyMQnwWC723nab8C",
-                  access_token_secret="dL9WMpKanfjYX8VutQcMtGudU0r3EHJajxP64ObOsQtAG")
-
+api = twitter.Api(consumer_key, consumer_secret, access_token_key, access_token_secret)
 previousTweets = []
 
 
-def initiate_loop():
-    print("Status: Let the paella party begin!")
-    while True:
-        data = api.GetUserTimeline(350695779)
-        for status in data:
-            tweetText = status.text
-            if tweetText[0] != "@":
-                if "Paella" in tweetText or "paella" in tweetText:
-                    if status.id not in previousTweets:
-                        # send twilio text message to phone
-                        twilioCli.messages.create(body=status.text, from_=myTwilNum, to=myCellNum)
-                        print(status.text)
-                        # add tweet id to previousTweets list
-                        previousTweets.append(status.id)
-                        print("Added status id: ", status.id, "to previousTweets.")
-        time.sleep(300)
+class PaellaParty():
+
+    def __init__(self):
+        self.twittername = input("Enter twitter id to monitor: ")
+        self.monitortext = input("Enter keyword to monitor for: ")
+        self.initiate_loop(self.twittername, self.monitortext)
+
+    def initiate_loop(self, twittername, monitortext):
+        print("Status: Let the paella party begin!")
+        while True:
+            data = api.GetUserTimeline(twittername)
+            for status in data:
+                tweetText = status.text
+                if tweetText[0] != "@":
+                    if monitortext in tweetText:
+                        if status.id not in previousTweets:
+                            # paella party found - send twilio text message to phone
+                            twilioCli.messages.create(body=status.text, from_=myTwilNum, to=myCellNum)
+                            print(status.text)
+                            # add tweet id to previousTweets list
+                            previousTweets.append(status.id)
+                            print("Added status id: ", status.id, "to previousTweets.")
+            # check again in 5 minutes for new tweet
+            time.sleep(300)
 
 
 if __name__ == "__main__":
-    initiate_loop()
+    start=PaellaParty()
